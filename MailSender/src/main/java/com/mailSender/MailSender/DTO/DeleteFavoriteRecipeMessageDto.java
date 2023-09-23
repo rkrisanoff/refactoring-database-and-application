0de8802e616fd147d4805a43589bdc8cdbd8e472
Anton <anton.shabalov@icloud.com>
2023-09-23 14:49:30 +0300

    del all

--- MailSender/src/main/java/com/mailSender/MailSender/DTO/DeleteFavoriteRecipeMessageDto.java
+++
@@ -1,7 +0,0 @@
-package com.mailSender.MailSender.DTO;
-import lombok.Data;
-
-@Data
-public class DeleteFavoriteRecipeMessageDto {
-    private String email;
-}
--- MailSender/src/main/java/com/mailSender/MailSender/service/interfaces/SendEmail.java
+++
@@ -1,7 +0,0 @@
-package com.mailSender.MailSender.service.interfaces;
-
-import com.mailSender.MailSender.DTO.SetApproveRecipeMessageDto;
-
-public interface SendEmail {
-    void sendMail(final SetApproveRecipeMessageDto setApproveRecipeMessageDto);
-}
